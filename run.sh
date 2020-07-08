7815e117d34776df6b0633ecae0d82b56874f98f
lune* <lune@wuvu.net>
2020-07-08 17:31:26 +0900

    Fix jupyter's token not being set on Windows

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 
 /usr/sbin/sshd
-jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token='jupyter'
-
+jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token="jupyter"
